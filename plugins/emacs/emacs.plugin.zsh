7e70dab2afa4bad1050da5274ec0499a4918780c
Tristan Carel <tristan.carel@gmail.com>
2011-04-13 01:36:37 +0200

    Use plugin require_tool to make sure at least emacs 23 is installed.

--- plugins/emacs/emacs.plugin.zsh
+++ plugins/emacs/emacs.plugin.zsh
@@ -1,11 +1,20 @@
-if type emacs >/dev/null 2>&1 ; then
-    if emacs --version | grep -q "^GNU Emacs 23" ;then
-	export EDITOR="$ZSH/plugins/emacs/emacsclient.sh"
-	alias emacs="$EDITOR --no-wait"
-	alias e=emacs
-	alias emacs_frame='emacsclient --alternate-editor "" --create-frame'
-    fi
+if ! type require_tool >/dev/null 2>&1 ; then
+    cat <<EOF >/dev/stderr
+Error: emacs plugin requires plugin require_tool.
+Please enable it in your ~/.zshrc
+EOF
+    read
+    exit 1
 fi
 
-alias emasc=emacs
-alias emcas=emacs
+# Use daemon capabilities of emacs 23
+if require_tool emacs 23 2>/dev/null ; then
+    export EDITOR="$ZSH/plugins/emacs/emacsclient.sh"
+    alias emacs="$EDITOR --no-wait"
+    alias e=emacs
+    # night is long
+    alias emasc=emacs
+    alias emcas=emacs
+    # create a new X frame
+    alias emacs_frame='emacsclient --alternate-editor "" --create-frame'
+fi
