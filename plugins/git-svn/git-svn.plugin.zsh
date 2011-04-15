1f21d5635be28b9880d1bba813dfa6e1febd2a80
Tristan Carel <tcarel@exalead.com>
2011-04-15 16:47:46 +0200

    Use my fork of git-svn-clone... fix path where git repository is downloaded

--- plugins/git-svn/git-svn.plugin.zsh
+++ plugins/git-svn/git-svn.plugin.zsh
@@ -1,6 +1,7 @@
 
 if ! [ -d "$ZSH/plugins/git-svn/git-svn-clone-externals" ] ;then
-    git clone https://github.com/andrep/git-svn-clone-externals.git
+    git clone https://github.com/tristan0x/git-svn-clone-externals.git \
+        "$ZSH/plugins/git-svn/git-svn-clone-externals"
 fi
 export PATH="$ZSH/plugins/git-svn/git-svn-clone-externals:$PATH"
 
